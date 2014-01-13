768df630e31786fcb3ef78787184abaa8ea14c3e
Txiki <iam@weird.es>
2014-01-13 17:03:33 +0100

    indexTitle

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>weird</title>
+    <title>dem0</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="dem0">
     <meta name="author" content="www.weird.es">
